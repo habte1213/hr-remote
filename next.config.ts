273b661e1dc6fa01c18bab98b9ffe3c72b7bfba7
habte1213 <155944753+habte1213@users.noreply.github.com>
2025-03-26 07:39:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+import { FederatedTypesPlugin } from "@module-federation/typescript";
+
+const federatedConfig:any = {
+  name: "hr-remote",
+  filename: "static/chunks/remoteEntry.js",
+  exposes: {
+    "./Home": "./src/components/Home.tsx",
+  },
+  shared: {},
+};
+
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack(config:any, options:any) {
+    config.plugins.push(
+      new NextFederationPlugin(federatedConfig),
+      new FederatedTypesPlugin({ federationConfig: federatedConfig })
+    );
+    return config;
+  },
+};
+
+export default nextConfig;
